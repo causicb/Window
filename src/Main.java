ceeedbe10140aa7e18f66ffec98be266ca64f48c
causicb <benjo1306@gmail.com>
2025-04-11 20:17:33 +0200

    get, set

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,23 @@ public class Main {
         Window window = new Window("window1");
         System.out.println("Area is: " + window.area());
         System.out.println("Perimeter is: " + window.perimeter());
+        window.setActiveStatus(true);
         System.out.println(window);
         window.draw();
+
+        Window window2 = new Window(
+                "window2",
+                "w2",
+                10, 50, 50, 10,
+                5, 5, 20, 20,
+                "blue",
+                true
+        );
+        window2.setColor("red");
+        window2.setActiveStatus(false);
+        window2.getActiveStatus();
+        System.out.println(window2);
+        window2.draw();
 
     }
 
--- src/Window.java
+++ src/Window.java
@@ -29,6 +29,117 @@ public class Window {
         coordinatesChecker();
     }
 
+    public Window(String title, String tag, int x1, int x2, int x3, int x4, int y1, int y2, int y3, int y4, String color, boolean isActive) {
+        this.title = title;
+        this.tag = tag;
+        this.x1 = x1;
+        this.x2 = x2;
+        this.x3 = x3;
+        this.x4 = x4;
+        this.y1 = y1;
+        this.y2 = y2;
+        this.y3 = y3;
+        this.y4 = y4;
+        this.color = color;
+        this.isActive = isActive;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public int getX1() {
+        return x1;
+    }
+
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public void setX2(int x2) {
+        this.x2 = x2;
+    }
+
+    public int getX3() {
+        return x3;
+    }
+
+    public void setX3(int x3) {
+        this.x3 = x3;
+    }
+
+    public int getX4() {
+        return x4;
+    }
+
+    public void setX4(int x4) {
+        this.x4 = x4;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public void setY1(int y1) {
+        this.y1 = y1;
+    }
+
+    public int getY2() {
+        return y2;
+    }
+
+    public void setY2(int y2) {
+        this.y2 = y2;
+    }
+
+    public int getY3() {
+        return y3;
+    }
+
+    public void setY3(int y3) {
+        this.y3 = y3;
+    }
+
+    public int getY4() {
+        return y4;
+    }
+
+    public void setY4(int y4) {
+        this.y4 = y4;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean getActiveStatus() {
+        return isActive;
+    }
+
+    public void setActiveStatus(boolean active) {
+        isActive = active;
+    }
+
     public void coordinatesChecker() {
         int[] xs = {x1, x2, x3, x4};
         int[] ys = {y1, y2, y3, y4};
